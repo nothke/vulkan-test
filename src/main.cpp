6da5f4a58fdca22e59677e51a6acc904cc5878df
Ivan Notaros <nothke@gmail.com>
2021-03-05 12:44:27 +0100

    Creating a logical device

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,8 @@ const bool enableValidationLayers = true;
 // Vulkan
 VkInstance instance;
 VkDebugUtilsMessengerEXT debugMessenger;
+VkPhysicalDevice physicalDevice{ VK_NULL_HANDLE };
+VkDevice device;
 
 const std::vector<const char*> validationLayers = {
 	"VK_LAYER_KHRONOS_validation"
@@ -169,7 +171,6 @@ bool IsDeviceSuitable(VkPhysicalDevice device)
 
 void PickPhysicalDevice()
 {
-	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 
 	uint32_t deviceCount = 0;
 	vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
@@ -193,6 +194,45 @@ void PickPhysicalDevice()
 
 	if (physicalDevice == VK_NULL_HANDLE) {
 		throw std::runtime_error("failed to find a suitable GPU!");
+	}
+}
+
+void CreateLogicalDevice()
+{
+	QueueFamilyIndices indices = FindQueueFamilies(physicalDevice);
+
+	VkDeviceQueueCreateInfo queueCreateInfo{};
+	queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+	queueCreateInfo.queueCount = 1;
+
+	float queuePriority = 1.0f;
+	queueCreateInfo.pQueuePriorities = &queuePriority;
+
+	VkPhysicalDeviceFeatures deviceFeatures{}; // Define which features you want
+
+	// Actually create the device
+	VkDeviceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+	createInfo.pQueueCreateInfos = &queueCreateInfo;
+	createInfo.queueCreateInfoCount = 1;
+
+	createInfo.pEnabledFeatures = &deviceFeatures;
+
+	// Extensions
+	createInfo.enabledExtensionCount = 0;
+
+	if (enableValidationLayers) {
+		createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+		createInfo.ppEnabledLayerNames = validationLayers.data();
+	}
+	else {
+		createInfo.enabledLayerCount = 0;
+	}
+
+	if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+		throw std::runtime_error("failed to create logical device!");
 	}
 }
 
@@ -265,6 +305,8 @@ int main(int argc, char* argv[])
 
 	PickPhysicalDevice();
 
+	CreateLogicalDevice();
+
 	// MAIN LOOP
 	while (!glfwWindowShouldClose(window))
 	{
@@ -272,6 +314,8 @@ int main(int argc, char* argv[])
 	}
 
 	// Cleanup
+	vkDestroyDevice(device, nullptr);
+
 	if (enableValidationLayers) {
 		DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
 	}
